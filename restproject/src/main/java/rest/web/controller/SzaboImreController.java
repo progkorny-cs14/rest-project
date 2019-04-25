ed06b8ac291c517629dbf0d95994e058ce14f640
Imre <thanatosz@mailbox.hu>
2019-04-25 13:26:44 +0200

    Gyakorlat Branch

---
+++ restproject/src/main/java/rest/web/controller/SzaboImreController.java
@@ -0,0 +1,22 @@
+package rest.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rest.entity.User;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/szi")
+@Slf4j
+public class SzaboImreController {
+
+    @GetMapping("")
+    public User getUser() {
+        log.info("process=get-users");
+        return new User("Szabó Imre", "96.sz.imre@gmail.com", LocalDateTime.now(), LocalDateTime.now());
+    }
+
+}
