af52af4a7c7908fe4f34ee877634634dce4d37e0
Czeglédi Attila <czegiati@gmail.com>
2019-05-02 12:50:35 +0200

    commit1

--- restproject/src/main/java/rest/web/controller/CzeglediAttilaController.java
+++ restproject/src/main/java/rest/web/controller/CzeglediAttilaController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/api/fk")
 @Slf4j
-public class KornelFilepController {
+public class CzeglediAttilaController {
 
     @GetMapping("")
     public User getUser() {
